77410f7b269a929bd927304cc7eb1d527b787de7
Matthias Diener <mdiener@inf.ufrgs.br>
2012-10-08 19:06:46 -0300

    t

--- pt_pf_thread.c
+++ pt_pf_thread.c
@@ -17,7 +17,7 @@ int pt_pf_func(void* v)
 	while (1) {
 		if (kthread_should_stop())
 			return 0;
-		// pt_detect_app();
+		pt_detect_app();
 		if (pt_task && pid_alive(pt_task) && pt_task->mm)
 			pt_check_next_addr(pt_task->mm);
 		msleep(10);
