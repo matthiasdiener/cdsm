1a1608375b1ef74dbb374dac1ca7a698af48f338
Matthias Diener <mdiener@inf.ufrgs.br>
2012-10-23 13:21:03 -0200

    t

--- pt_comm.c
+++ pt_comm.c
@@ -86,7 +86,7 @@ int pt_pte_fault(struct task_struct *task, struct mm_struct *mm,
 	return 0; /* not reached */
 }
 
-struct jprobe pt_jprobe = {
+struct jprobe pt_ptef_jprobe = {
 	.entry = (kprobe_opcode_t *) pt_pte_fault
 };
 
@@ -137,11 +137,11 @@ int init_module(void)
 	spcd_exit_process_original_ref = spcd_exit_process;
 	spcd_exit_process = &spcd_exit_process_new;
 
-	pt_jprobe.kp.addr = (kprobe_opcode_t *) kallsyms_lookup_name("handle_pte_fault"); //not necessary
+	pt_ptef_jprobe.kp.addr = (kprobe_opcode_t *) kallsyms_lookup_name("handle_pte_fault"); //not necessary
 	pt_dpf_probe.addr = (kprobe_opcode_t *) kallsyms_lookup_name("do_page_fault") + 0x5D;
 
 	register_kprobe(&pt_dpf_probe);
-	register_jprobe(&pt_jprobe);
+	register_jprobe(&pt_ptef_jprobe);
 
 	return 0;
 }
@@ -151,7 +151,8 @@ void cleanup_module(void)
 {
 	spcd_new_process = spcd_new_process_original_ref;
 	spcd_exit_process = spcd_exit_process_original_ref;
-	unregister_jprobe(&pt_jprobe);
+	unregister_jprobe(&pt_ptef_jprobe);
+	unregister_kprobe(&pt_dpf_probe);
 	printk("Bye.....\n");
 }
 
