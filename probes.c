da3e6739ab515b985e5d77881e558ad08e726dc6
Matthias Diener <mdiener@inf.ufrgs.br>
2012-11-28 18:07:48 -0200

    optimize

--- probes.c
+++ probes.c
@@ -40,13 +40,11 @@ static inline int spcd_check_name(char *name)
 	
 	int i, len = sizeof(bm_names)/sizeof(bm_names[0]);
 
-	if (pt_task)
-		return 0;
-
 	for (i=0; i<len; i++) {
 		if (strstr(name, bm_names[i]))
 			return 1;
 	}
+
 	return 0;
 }
 
