52c76c1fbcce71f5971f092e75b52a4981fff24f
Matthias Diener <mdiener@inf.ufrgs.br>
2013-02-18 17:19:55 -0300

    add mapping param; version information

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 obj-m := spcd.o
-spcd-objs := pagefault.o mem.o pid.o mem.o sharing.o probes.o intercept.o spcd_main.o
+spcd-objs := pagefault.o mem.o pid.o mem.o sharing.o probes.o intercept.o map.o topo.o spcd_main.o
 
+SPCD_VER=$(shell git describe)
+DATE=$(shell date)
 
 .PHONY: all clean
 
 all:
+	@echo "#define SPCD_VERSION \"$(SPCD_VER); $(DATE)\"" > version.h
 	@if stat -t obj/* >/dev/null 2>&1; then mv -f obj/* . ; else mkdir -p obj; fi
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 	@mv -f *.o modules.order Module.symvers spcd.mod.c obj
--- spcd_main.c
+++ spcd_main.c
@@ -1,20 +1,24 @@
 #include "spcd.h"
+#include "version.h"
 
 MODULE_LICENSE("GPL");
 
 #define NUM_FAULTS_DEFAULT 3
 
 static struct task_struct *pf_thread;
-// static struct task_struct *map_thread;
+static struct task_struct *map_thread;
 
 int num_faults = NUM_FAULTS_DEFAULT;
+int do_map = 0;
 module_param(num_faults, int, 0);
+module_param(do_map, int, 0);
 
 int init_module(void)
 {
-	printk("SPCD: Welcome.....\n");
-	printk("SPCD: num_faults: %d %s\n", num_faults, num_faults==NUM_FAULTS_DEFAULT ? "(default)" : "");
-	printk("SPCD: max_threads: %d\n", PT_MAXTHREADS);
+	printk("SPCD: Start (version %s)\n", SPCD_VERSION);
+	printk("\tnum_faults: %d %s\n", num_faults, num_faults==NUM_FAULTS_DEFAULT ? "(default)" : "");
+	printk("\tmax_threads: %d\n", PT_MAXTHREADS);
+	printk("\tuse mapping: %s\n", do_map ? "yes" : "no");
 
 
 	reset_stats();
@@ -23,12 +27,14 @@ int init_module(void)
 	pf_thread = kthread_create(spcd_pagefault_func, NULL, "spcd_pf_thread");
 	wake_up_process(pf_thread);
 
-	// map_thread = kthread_create(spcd_map_func, NULL, "spcd_map_thread");
-	// wake_up_process(map_thread);
+	if (do_map) {
+		map_thread = kthread_create(spcd_map_func, NULL, "spcd_map_thread");
+		wake_up_process(map_thread);
+	}
 
 	interceptor_start();
 
-	// topo_init();
+	topo_init();
 
 	return 0;
 }
@@ -39,13 +45,13 @@ void cleanup_module(void)
 	if (pf_thread)
 		kthread_stop(pf_thread);
 
-	// if (map_thread)
-	// 	kthread_stop(map_thread);
+	if (map_thread)
+		kthread_stop(map_thread);
 
 	unregister_probes();
 	interceptor_end();
 
-	// topo_cleanup();
+	topo_cleanup();
 
 	printk("Bye.....\n");
 }
