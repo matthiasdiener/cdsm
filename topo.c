4d98aeb783a6c8f7bdf474dc9fcce22b04cd3a4d
Matthias Diener <mdiener@inf.ufrgs.br>
2013-02-28 17:36:03 -0300

    correct topo

--- topo.c
+++ topo.c
@@ -2,41 +2,40 @@
 #include <linux/cpu.h>
 
 #define for_each_sibling(s, cpu) for_each_cpu(s, cpu_sibling_mask(cpu))
-
-static int nodes[16];
-static int cpus[16];
-// static int cores[64];
-
-
+ 
 void topo_init(void)
 {
-	unsigned long node;
-	unsigned long cpu;
+	unsigned long node, cpu, sibling;
 	int curCPU = 0;
-	// unsigned long sibling;
 
-	for_each_online_node(node){
-		nodes[node] = 1;
+	for_each_online_node(node) {
+		int cpus[64] = {};
+		int cores[64] = {};
+
 		printk("node: %lu\n", node);
-		for_each_present_cpu(cpu) {
+
+		for_each_online_cpu(cpu) {
 			int physID = cpu_data(cpu).phys_proc_id;
+			if (cpu_to_node(cpu) != node)
+				continue;
 			if (!cpus[physID]) {
 				printk("  processor: %d\n", physID);
 				cpus[physID] = 1;
 				curCPU = physID;
 			}
-			if (physID == curCPU)
-				printk("    core: %lu\n", cpu);
+			if (physID == curCPU && !cores[cpu]) {
+				printk("    core: %2lu", cpu);
+				cores[cpu] = 1;
+				for_each_sibling(sibling, cpu) {
+					if (!cores[sibling]) {
+						printk(", %2lu", sibling);
+						cores[sibling] = 1;
+					}
+				}
+				printk("\n");
+			}
 		}
 	}
-
-	// for_each_present_cpu(cpu) {
-	// 	node = cpu_to_node(cpu);
-	// 	printk("cpu: %lu, node: %lu\n", cpu, node);
-	// 	for_each_sibling(sibling, cpu) {
-	// 		printk("\tsibling: %lu\n", sibling);
-	// 	}
-	// }
 }
 
 
