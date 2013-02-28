159527d0d616a8cc7db5eb52244a8f3ea60a5802
Matthias Diener <mdiener@inf.ufrgs.br>
2013-02-28 16:21:33 -0300

    topo

--- topo.c
+++ topo.c
@@ -3,49 +3,44 @@
 
 #define for_each_sibling(s, cpu) for_each_cpu(s, cpu_sibling_mask(cpu))
 
-int nr_node;
-int nr_cpu;
-int nr_sibling;
+static int nodes[16];
+static int cpus[16];
+// static int cores[64];
 
-enum tree_node_type {
-	NUMA = 1,
-	CPU,
-	SIBLING,
-};
-
-struct tree_node {
-	int id;
-	struct tree_node *parent;
-	struct tree_node *firstchild;
-	struct tree_node *nextsibling;
-};
-
-struct tree_node *root;
 
 void topo_init(void)
 {
 	unsigned long node;
 	unsigned long cpu;
-	unsigned long sibling;
+	int curCPU = 0;
+	// unsigned long sibling;
 
-	root = kmalloc(sizeof (struct tree_node), GFP_KERNEL);
-
-	// for_each_online_node(node){
-	// 	printk("node: %lu\n", node);
-	// }
-
-	for_each_present_cpu(cpu) {
-		node = cpu_to_node(cpu);
-		printk("cpu: %lu, node: %lu\n", cpu, node);
-		for_each_sibling(sibling, cpu) {
-			printk("\tsibling: %lu\n", sibling);
+	for_each_online_node(node){
+		nodes[node] = 1;
+		printk("node: %lu\n", node);
+		for_each_present_cpu(cpu) {
+			int physID = cpu_data(cpu).phys_proc_id;
+			if (!cpus[physID]) {
+				printk("  processor: %d\n", physID);
+				cpus[physID] = 1;
+				curCPU = physID;
+			}
+			if (physID == curCPU)
+				printk("    core: %lu\n", cpu);
 		}
 	}
+
+	// for_each_present_cpu(cpu) {
+	// 	node = cpu_to_node(cpu);
+	// 	printk("cpu: %lu, node: %lu\n", cpu, node);
+	// 	for_each_sibling(sibling, cpu) {
+	// 		printk("\tsibling: %lu\n", sibling);
+	// 	}
+	// }
 }
 
 
 
 void topo_cleanup(void)
 {
-	kfree(root);
 }
