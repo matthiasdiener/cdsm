5637db4387712444193e323188a659ef71733b48
Matthias Diener <mdiener@inf.ufrgs.br>
2013-03-15 16:47:29 -0300

    restore static benchmark name detection

--- probes.c
+++ probes.c
@@ -31,11 +31,15 @@ static inline void print_stats(void)
 
 static inline int spcd_check_name(char *name)
 {
-	//[TODO] possible bottleneck
-	int i, len;	
-	char **bm_names;
-	
-	len = spcd_procfs_get_names(&bm_names);
+	const char *bm_names[] = {".x", /*NAS*/
+	"blackscholes", "bodytrack", "facesim", "ferret", "freqmine", "rtview", "swaptions", "fluidanimate", "vips", "x264", "canneal", "dedup", "streamcluster", /*Parsec*/
+	"LU", "FFT", "CHOLESKY", /*Splash2*/
+	"wupwise_", "swim_", "mgrid_", "applu_", "galgel_", "equake_", "apsi_", "gafort_", "fma3d_", "art_", "ammp_", /* Spec OMP 2001 */
+	"md_omp_", "bwaves_", "nabmd_", "bt_", "bots-alignment_", "bots-sparselu_", "ilbdc_", /*"fma3d", "swim_", */ "convert_", "mg_", "lu_", "smithwaterman_", "kdtree_", /* Spec OMP 2012 */
+	};
+
+	int i, len = sizeof(bm_names)/sizeof(bm_names[0]);
+	// len = spcd_procfs_get_names(&bm_names);
 
 	for (i=0; i<len; i++) {
 		if (strstr(name, bm_names[i]))
