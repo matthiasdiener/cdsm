bfa96b85b2baaed245d049b3e99456d3a0889e38
Matthias Diener <mdiener@inf.ufrgs.br>
2013-03-22 12:31:52 -0300

    test optimize pagefault

--- pagefault.c
+++ pagefault.c
@@ -29,24 +29,24 @@ int spcd_pagefault_func(void* v)
 }
 
 
-static int pt_callback_page_walk(pte_t *pte, unsigned long addr, unsigned long next_addr, struct mm_walk *walk)
+static inline int pt_callback_page_walk(pte_t *pte, unsigned long addr, unsigned long next_addr, struct mm_walk *walk)
 {
-	pgd_t *pgd;
-	pud_t *pud;
-	pmd_t *pmd;
-	spinlock_t *myptl;
+	// pgd_t *pgd;
+	// pud_t *pud;
+	// pmd_t *pmd;
+	// spinlock_t *myptl;
 	
 	if (pte_none(*pte) || !pte_present(*pte) || !pte_young(*pte) || pte_special(*pte))
 		return 0;
 
-	pgd = pgd_offset(walk->mm, addr);
-	pud = pud_offset(pgd, addr);
-	pmd = pmd_offset(pud, addr);
+	// pgd = pgd_offset(walk->mm, addr);
+	// pud = pud_offset(pgd, addr);
+	// pmd = pmd_offset(pud, addr);
 	pt_next_addr = addr;
 
-	pte = pte_offset_map_lock(walk->mm, pmd, addr, &myptl);
+	// pte = pte_offset_map_lock(walk->mm, pmd, addr, &myptl);
 	*pte = pte_clear_flags(*pte, _PAGE_PRESENT);
-	pte_unmap_unlock(pte, myptl);
+	// pte_unmap_unlock(pte, myptl);
 	
 	pt_pf_extra++;
 
