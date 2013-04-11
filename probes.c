c202a7a4a654ec62e467fe14c9c409a6adcef700
Matthias Diener <mdiener@inf.ufrgs.br>
2013-04-11 13:34:07 -0300

    t

--- probes.c
+++ probes.c
@@ -79,9 +79,11 @@ void spcd_pte_fault_handler(struct mm_struct *mm,
 		pt_pf++;
 		if (is_shared(find_vma(mm, address))) {
 			unsigned long physaddr = pte_pfn(*pte);
-			pt_check_comm(tid, physaddr<<(PAGE_SHIFT) );
-			// pt_check_comm(tid, (physaddr<<PAGE_SHIFT) | (address & (PAGE_SIZE-1)));
-			printk("addr:%lx physaddr: %lx\n", address, physaddr);
+			unsigned long finaladdr = (physaddr<<PAGE_SHIFT) | (address & (PAGE_SIZE-1));
+			if (physaddr)
+			//pt_check_comm(tid, physaddr<<(PAGE_SHIFT) );
+			pt_check_comm(tid, (physaddr<<PAGE_SHIFT) | (address & (PAGE_SIZE-1)));
+			printk("addr:%lx physaddr: %lx, finaladdr: %lx\n", address, physaddr, finaladdr);
 		}
 	}
 
