f32e2bb5d29687a0e29397ca0f55cfe9647f10ba
Matthias Diener <mdiener@inf.ufrgs.br>
2013-08-25 20:05:18 -0300

    fix whitespace

--- probes.c
+++ probes.c
@@ -177,7 +177,7 @@ void spcd_process_handler(struct task_struct *tsk)
 {
 	int at, tid = spcd_get_tid(tsk->pid);
 
-	if (tid > -1 && tsk->flags & PF_EXITING) {		
+	if (tid > -1 && tsk->flags & PF_EXITING) {
 		spcd_delete_pid(tsk->pid);
 		at = spcd_get_active_threads();
 		printk("SPCD: %s stop (pid %d, tid %d), #active: %d\n", tsk->comm, tsk->pid, tid, at);
@@ -187,7 +187,7 @@ void spcd_process_handler(struct task_struct *tsk)
 			reset_stats();
 		}
 		jprobe_return();
-	} 
+	}
 
 	if (check_name(tsk->comm) && tid == -1 && !(tsk->flags & PF_EXITING)) {
 		tid = spcd_add_pid(tsk->pid);
